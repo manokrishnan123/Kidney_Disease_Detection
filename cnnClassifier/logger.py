b19687e3f7171378513098fbac768123fca30ad5
manokrishnan123 <manobk08@gmail.com>
2025-03-10 13:05:00 +0000

    Input Data Ingestion stage completed

--- cnnClassifier/logger.py
+++ cnnClassifier/logger.py
@@ -2,21 +2,23 @@ import os
 import sys
 import logging
 
+# Define logging format
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 
+# Create logs directory if not exists
 log_dir = "logs"
-log_filepath = os.path.join(log_dir,"runtime_logs.log")
+log_filepath = os.path.join(log_dir, "runtime_logs.log")
 os.makedirs(log_dir, exist_ok=True)
 
-
+# Configure logging
 logging.basicConfig(
-    level= logging.INFO,
-    format= logging_str,
-
+    level=logging.INFO,
+    format=logging_str,
     handlers=[
         logging.FileHandler(log_filepath),
         logging.StreamHandler(sys.stdout)
     ]
 )
 
+# Define the logger
 logger = logging.getLogger("cnnClassifierLogger")
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from cnnClassifier.logger import logger
+from cnnClassifier.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Starting the application...")
+
+STAGE_NAME = "Data Ingestion"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e

3bec5819ab9b143a2777cac39d1e43161a6fa637
manokrishnan123 <manobk08@gmail.com>
2025-04-25 12:44:12 +0100

    final push and commit

--- research/trials.py
+++ research/trials.py
@@ -1,8 +1,3 @@
-'''import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
-print("Built with CUDA:", tf.test.is_built_with_cuda())
-print("GPUs available:", tf.config.list_physical_devices('GPU'))'''
-
 import torch
 
 print("Number of GPU: ", torch.cuda.device_count())
